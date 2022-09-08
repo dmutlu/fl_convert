e3efbd19e70b6c354ad445ca37c87ec8fa96c188
dmutlu <devin.mutlu+github@gmail.com>
2022-09-08 18:38:45 -0400

    Dead code cleanup, added error handleing for write_out.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ use bstr::{BString, ByteSlice};
 fn read_save(filename: &str) -> io::Result<BString> {
     let mut file: File = File::open(&filename.trim())?;
     let mut buffer: Vec<u8> = Vec::new();
-    let mut _buffer_debug: Vec<u8> = Vec::new();
         
-    //file.read_to_end(&mut buffer_debug)?;
     // Read the whole file
     file.read_to_end(&mut buffer)?;
 
     // Use a Byte String because FL saves are ANSI (Windows code page WinLatin1)
     let contents = BString::from(buffer);
-
-    //println!("{:?}", buffer_debug);
 
     Ok(contents)
 }
@@ -24,13 +20,6 @@ fn decrypt (buffer: BString) -> io::Result<String> {
     let gene = [0x0047, 0x0065, 0x006E, 0x0065];
     let my_buf = &buffer;
     let byte_buf_len = my_buf.len();
-    //let byte_buf = my_buf.as_bytes();
-
-    //for i in my_buf.into_iter() {
-    //    print!("{}", i);
-    //}
-
-    //println!("Buffer length: {:?}", byte_buf_len);
 
     let mut decipher_buf: Vec<u8> = Vec::new();
 
@@ -47,25 +36,31 @@ fn decrypt (buffer: BString) -> io::Result<String> {
     
         let decipher_save = std::str::from_utf8(&decipher_buf);
 
-        //println!("{:?}", my_buf);
-        //println!("Start of Conversion.");
-        //println!("{}", decipher_save.expect("msg"));
-
-        //write_out(decipher_save.unwrap_or("borked").to_string());
-
         // Return the deciphered save data.
         Ok(decipher_save.unwrap().to_string())
-    } else {
-        Err(exit(0))
+
+    } else { // Not encrypted.
+        exit(0)
     }
 }
 
 fn write_out(buf: String) -> io::Result<()> {
     let mut buffer = File::create("D:\\Users\\Devin\\source\\tmp\\my_save.fl")?;
-    //let mut w = Vec::new();
+
     write!(buffer, "{}", buf)?;
     Ok(())
 }
+
+/*
+TODO:
+    * Replace prompt with CLI args.
+    * Get current save file name for use in new out file.
+    * Backup current file.
+    * Allow for dynamic save location.
+    * Add logic for unencrypted file.
+    * Edit buffer for delta and mission value.
+    * GUI
+*/
 
 fn main() {
     let mut fl_path = String::new();
@@ -76,7 +71,10 @@ fn main() {
 
     let _fl_save_crypt = read_save(&fl_path);
 
-    let fl_save = read_save(&fl_path);
+    let fl_save = read_save(&fl_path).unwrap();
 
-    write_out(decrypt(fl_save.unwrap()).unwrap());
+   match write_out(decrypt(fl_save).unwrap()) {
+        Err(e) => println!("{:?}", e),
+        _ => ()
+    }
 }
