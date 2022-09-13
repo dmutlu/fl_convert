0f53f548adadc7e2031661968f400ed54557cc25
dmutlu <devin.mutlu+github@gmail.com>
2022-09-12 21:25:30 -0400

    Added save backup func.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{fs::File, io::{Read, self, Write, stdin}, convert::TryInto, env::{current_dir}, path::{PathBuf}, str::FromStr};
+use std::{fs::{File, self}, io::{Read, self, Write, stdin}, convert::TryInto, env::current_dir, path::{PathBuf}, str::FromStr, process::{exit}};
 use bstr::{BString, ByteSlice};
+use chrono::{DateTime, Utc};
 use regex::{Regex, Captures};
 
 fn read_save(filename: &str) -> io::Result<BString> {
@@ -10,21 +11,21 @@ fn read_save(filename: &str) -> io::Result<BString> {
     file.read_to_end(&mut buffer)?;
 
     // Use a Byte String because FL saves are ANSI (Windows code page WinLatin1).
-    let contents = BString::from(buffer);
+    let contents: BString = BString::from(buffer);
 
     Ok(contents)
 }
 
 fn decrypt (buffer: BString) -> io::Result<String> {
     // First 4 bytes of the file "FLS1" to skip.
-    let mut len =  4;
-    let mut my_iter = 0;
+    let mut len: usize =  4;
+    let mut my_iter: usize = 0;
 
-    // "Gene, Gene, The Cinnabon Machine"
-    let gene = [0x0047, 0x0065, 0x006E, 0x0065];
+    // "Gene, Gene, The Cinnabon Machine."
+    let gene: [usize; 4] = [0x0047, 0x0065, 0x006E, 0x0065];
 
-    let my_buf = &buffer;
-    let byte_buf_len = my_buf.len();
+    let my_buf: &BString = &buffer;
+    let byte_buf_len: usize = my_buf.len();
     let mut decipher_buf: Vec<u8> = Vec::new();
 
     // Check for encrypted save.fl file header.
@@ -49,13 +50,19 @@ fn decrypt (buffer: BString) -> io::Result<String> {
 }
 
 fn fix_save(buf: String) -> String {
+    // Match 'MissionNum' line, group assigned value.
     let re: Regex = Regex::new(r"MissionNum.*(.+)").unwrap();
-
+    // Capture the 'MissionNum' line from the save.
     let mission_cap: Captures = re.captures(&buf).expect("The 'MissionNum' parameter was not found.");
+    // Save the entire line as a str.
     let mission_orig_str: &str = mission_cap.get(0).unwrap().as_str();
+    // Save just the current value as a str.
     let mission_num_str: &str = mission_cap.get(1).unwrap().as_str();
+    // Convert the value from str to i32.
     let mission_num: i32 = FromStr::from_str(mission_num_str).unwrap();
+    // Increment the value by 1 to allow player to advance to the next mission.
     let mission_inc: i32 = mission_num + 1;
+    // Replace the mission value in the original str with our new incremented value.
     let new_mission: String = mission_orig_str.replace(mission_num_str, mission_inc.to_string().as_str());
     
     let delta: &str = "delta_worth = -1.000000";
@@ -75,13 +82,36 @@ fn write_out(save_dir: PathBuf, buf: String) -> io::Result<()> {
     Ok(())
 }
 
-fn save_prompt(usr_ans: &mut String, pwd: PathBuf, decrypted_save: String, fix: bool) {
-    loop {
-        println!("");
-        println!("Save new file in current directory? (Y/N):");
-        stdin().read_line(usr_ans).expect("Cannot read input.");
+fn backup_save(fl_path: String) {
+    let now: DateTime<Utc> = Utc::now();
+    // Get the original save's path.
+    let orig_path: PathBuf = PathBuf::from(fl_path.trim());
+    // Get just the name for the original save.
+    let fl_name = orig_path.file_name();
+    let my_path: PathBuf = PathBuf::from(fl_name.unwrap());
+    let fl_date: String = format!("fl.{}.orig", now.format("%Y%m%d_%H%M%S"));
+    let fl_backup: PathBuf = my_path.with_extension(fl_date);
+
+    println!();
+    println!("Making backup of original save.");
+    fs::copy(orig_path, &fl_backup).expect("Unable to create backup of save file.");
+    println!("Backup complete: {}", fl_backup.display());
+}
+
+fn fl_options(fl_path: String, pwd: PathBuf, fl_save: BString, fix: bool) {
+    let mut usr_ans: String = String::new();
+    let decrypted_save: String = decrypt(fl_save).expect("Unable to read save contents to buffer.");
     
-        if usr_ans.to_lowercase().contains("y") || usr_ans.to_lowercase().contains("yes") {
+    backup_save(fl_path);
+             
+    loop {
+        println!();
+        println!("Save new file in current directory? (Y/N):");
+        stdin().read_line(&mut usr_ans).expect("Cannot read input.");
+    
+        let usr_ans: String = usr_ans.to_lowercase();
+
+        if usr_ans.contains('y') || usr_ans.contains("yes") {
             if fix {
                 if let Err(e) = write_out(pwd, fix_save(decrypted_save)) { println!("{:?}", e) }
                 break;
@@ -89,7 +119,7 @@ fn save_prompt(usr_ans: &mut String, pwd: PathBuf, decrypted_save: String, fix: 
                 if let Err(e) = write_out(pwd, decrypted_save) { println!("{:?}", e) }
                 break;
             }
-        } else if usr_ans.to_lowercase().contains("n") || usr_ans.to_lowercase().contains("no") {
+        } else if usr_ans.contains('n') || usr_ans.contains("no") {
             break;
         } else {
             println!("Answer must be \"Y, Yes, N, or No\".");
@@ -99,10 +129,10 @@ fn save_prompt(usr_ans: &mut String, pwd: PathBuf, decrypted_save: String, fix: 
 
 /*
 TODO:
-    * Get current save file name for use in new out file.
-    * Backup current file.
-    * Allow for dynamic save location.
-    * GUI
+    * Get current save file name for use in new out file. (0)
+    * Backup current file. (90)
+    * Allow for dynamic save location. (50)
+    * GUI (0)
 */
 
 fn main() {
@@ -114,21 +144,20 @@ fn main() {
     stdin().read_line(&mut fl_path).expect("Cannot read input.");
 
     let fl_save: BString = read_save(&fl_path).expect("Cannot find save file.");
-    let decrypted_save: String = decrypt(fl_save).unwrap();
 
-    //fix_save(decrypted_save);
-
-    println!("");
+    println!();
     println!("1. Convert Save");
     println!("2. Fix Save and/or Convert");
     println!("0. Exit");
     stdin().read_line(&mut usr_ans).expect("Cannot read input.");
 
-    if usr_ans.contains("1") {
-        save_prompt(&mut usr_ans, pwd, decrypted_save, false);
-    } else if usr_ans.contains("2") {
-        save_prompt(&mut usr_ans, pwd, decrypted_save, true);
+    if usr_ans.contains('1') {
+        fl_options(fl_path, pwd, fl_save, false);
+    } else if usr_ans.contains('2') {
+        fl_options(fl_path, pwd, fl_save, true);
+    } else if usr_ans.contains('0') {
+        exit(0);
     } else {
-        println!("Invalid Choice: {}", usr_ans)
+        println!("Invalid Choice: {}", usr_ans);
     }
 }
